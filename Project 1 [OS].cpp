437440ae9accbcbdaa7f0c77e8168db53756b8d9
TanejaAnish19 <49262360+TanejaAnish19@users.noreply.github.com>
2019-04-05 09:28:11 +0530

    Update Project 1 [OS].cpp

--- Project 1 [OS].cpp
+++ Project 1 [OS].cpp
@@ -8,6 +8,7 @@ int main()
  int a[20],b[20],n,i,j,temp,x;
  int head , pre_head;
  int sum = 0;
+ printf("*****************WELCOME TO SCAN DISK SCHEDULING PROGRAM******************\n");
 
  printf("Enter head pointer position:");
  scanf("%d",&a[0]);
@@ -17,7 +18,7 @@ int main()
 
  printf("\nEnter number of processes:");
  scanf("%d",&n);
- printf("\nEnter processes in request order");
+ printf("\nEnter processes in request order : \n");
  for(i=1;i<=n;i++)
  {
   scanf("%d",&a[i]);
@@ -36,4 +37,47 @@ int main()
    }
   }
  }
-
+ for(i=1;i<=n+1;i++)
+ {
+  if(head==a[i])
+  	x = i;
+ }
+ j=0;
+ if(head<pre_head)
+ {
+  for(i=x;i>0;i--)
+  {
+   sum+=(a[i]-a[i-1]);
+   b[j++]=a[i];
+  }
+  sum+=a[x+1]-a[0];
+  b[j++]=a[0];
+  for(i=x+1;i<n+1;i++)
+  {
+   sum+=(a[i+1]-a[i]);
+   b[j++]=a[i];
+  }
+  b[j++]=a[i];
+ }
+ else
+ {
+  for(i=x;i<n+2;i++)
+  {
+   sum+=(a[i+1]-a[i]);
+   b[j++]=a[i];
+  }
+  sum+=a[n+2]-a[x-1];
+  b[j++]=a[n+2];
+  for(i=x-1;i>1;i--)
+  {
+   sum+=(a[i]-a[i-1]);
+   b[j++]=a[i];
+  }
+  b[j++]=a[i];
+ }
+ printf("\nProcessing order : \n");
+ for(i=0;i<=n+1;i++)
+ printf("%d -->",b[i]);
+ printf("\nTotal Head Movement : %d",sum);
+ getch();
+}
